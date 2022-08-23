87d017d8d45909570e647834e003dcee8a0d806b
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-23 20:28:01 +0430

    feat: add genkey and check_consistency func

--- consistency-test.py
+++ consistency-test.py
@@ -1,4 +1,5 @@
 import os
+from random import random
 import time
 
 
@@ -17,5 +18,19 @@ class ConsistencyTester:
         self.cached = {}  # We take our view of data stored in the DB.
         time_stamp_us = time.time_ns() % 10**9 // 1000  # Get ns
         self.prefix = "|".join([str(os.getpid()), str(time_stamp_us),
-                       str(self.r.object_id), ""])
+                                str(self.r.object_id), ""])
         self.errtime = {}
+
+    def genkey(self):
+        # Write more often to a small subset of keys
+        ks = self.keyspace if random.random() > 0.5 else self.working_set
+        return self.prefix + "key_" + str(random.randint(0, ks))
+
+    def check_consistency(self, key, value):
+        expected = self.cached[key]
+        if not expected:  # We lack info about previous state.
+            return
+        if expected > value:
+            self.lost_writes += expected-value
+        elif expected < value:
+            self.not_ack_writes += value-expected
